f3144c94fa6f335a0553506c4c167a3ea55eb0d6
suvanbanerjee <banerjeesuvan@gmail.com>
2025-08-18 12:24:49 +0530

    fix build

--- cloudcasting_backend/web/application.py
+++ cloudcasting_backend/web/application.py
@@ -66,10 +66,10 @@ def get_app() -> FastAPI:
     # Add CORS middleware
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  # Allows all origins
+        allow_origins=["*"],
         allow_credentials=True,
-        allow_methods=["*"],  # Allows all methods
-        allow_headers=["*"],  # Allows all headers
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     # Main router for the API.
